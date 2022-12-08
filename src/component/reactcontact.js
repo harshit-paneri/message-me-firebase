6d1f099ce61bd0d6b439f03406e3cf35556606a3
Harshit Paneri <harshitpanari@gmail.com>
2022-12-08 11:22:37 +0530

    fronted check

--- src/component/reactcontact.js
+++ src/component/reactcontact.js
@@ -51,59 +51,74 @@ const Reactcontact = () => {
   };
   return (
     <>
-      <h1>Message from Techspeed</h1>
-      <form action="" method="POST">
-        <span>Message Me</span>
-        <div>
-          <span>Your Name</span>
-          <input
-            type="text"
-            name="name"
-            value={user.name}
-            onChange={getUserData}
-            id=""
-            required
-          />
-        </div>
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+        crossorigin="anonymous"
+      ></link>
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"
+      ></script>
+      <div className="all row">
+        <h1 className="head col-8">Message from Techspeed</h1>
 
-        <div>
-          <span>Your Email</span>
-          <input
-            type="text"
-            name="email"
-            value={user.email}
-            onChange={getUserData}
-            id=""
-          />
-        </div>
+        <form action="" method="POST" className="form col-6">
+          <span className="span">Message Me</span>
+          <div className="name">
+            <span>Your Name</span>
+            <input
+              type="text"
+              name="name"
+              className="inp"
+              value={user.name}
+              onChange={getUserData}
+              id=""
+              required
+            />
+          </div>
 
-        <div>
-          <span>Your Mobile</span>
-          <input
-            type="text"
-            name="mobile"
-            value={user.mobile}
-            onChange={getUserData}
-            id=""
-          />
-        </div>
+          <div>
+            <span>Your Email</span>
+            <input
+              type="text"
+              name="email"
+              value={user.email}
+              onChange={getUserData}
+              id=""
+            />
+          </div>
 
-        <div>
-          <span>Your Message</span>
-          <textarea
-            name="message"
-            value={user.message}
-            onChange={getUserData}
-            id=""
-          />
-        </div>
+          <div>
+            <span>Your Mobile</span>
+            <input
+              type="text"
+              name="mobile"
+              value={user.mobile}
+              onChange={getUserData}
+              id=""
+            />
+          </div>
 
-        <div>
-          <button onClick={postData}>
-            <span>Submit</span>
-          </button>
-        </div>
-      </form>
+          <div>
+            <span>Your Message</span>
+            <textarea
+              name="message"
+              value={user.message}
+              onChange={getUserData}
+              id=""
+            />
+          </div>
+
+          <div>
+            <button onClick={postData}>
+              <span>Submit</span>
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
